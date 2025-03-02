0fd5186b0c00fe3333c435929af8ec094be2976e
Serena Li <liserena@usc.edu>
2025-03-01 16:15:58 -0800

    apis

--- my-app/backend/main.py
+++ my-app/backend/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Form, HTTPException
 
 app = FastAPI()
 
@@ -12,10 +12,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/api/data")
 def get_data():
     return {"message": "Hello from FastAPI!"}
 
+
+@app.post("/get-origin")
+async def get_origin(origin: str = Form(...)):
+    if not origin.strip():
+        raise HTTPException(status_code=400, detail="Destination cannot be empty")
+    return {"received_message": origin}
+
+
+@app.post("/get-destination")
+async def get_destination(destination: str = Form(...)):
+    if not destination.strip():
+        raise HTTPException(status_code=400, detail="Destination cannot be empty")
+    return {"received_message": destination}
+
+
+@app.post("/get-interval")
+async def get_interval(time: int = Form(...)):
+    if time <= 0:
+        raise HTTPException(status_code=400, detail="Interval must be greater than 0")
+    return {"received_message": time}
+
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
