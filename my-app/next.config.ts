544dcf254a588e453ee824d2a74e1951bd6a43d0
Aurelisa Juan <aurelisa.sindhu@gmail.com>
2025-03-02 13:52:17 -0800

    remove extra config

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   ignoreBuildErrors: true,
+  images: {
+    domains: ["mt0.google.com"],
+  },
 };
 
 export default nextConfig;
